73f44bbbbd872da61a9ef1e880ccfbf13005c70a
Andrej Kolčin <self@kaathewise.net>
2025-09-03 12:32:28 +0300

    [redjot-parse] Use the `fromDjot` function

--- packages/redjot-parse/src/index.ts
+++ packages/redjot-parse/src/index.ts
@@ -1,13 +1,8 @@
 import * as djot from "@djot/djot"
 import type { Document } from "djast"
-import { fromDjotDoc } from "djast-util-from-djot"
+import { fromDjot } from "djast-util-from-djot"
 import type { Processor } from "unified"
 
 export default function redjotParse(this: Processor): void {
-	this.parser = parse
-}
-
-export function parse(document: string): Document {
-	const doc = djot.parse(document, { sourcePositions: true })
-	return fromDjotDoc(doc)
+	this.parser = fromDjot
 }
